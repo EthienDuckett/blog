6fe71c6e37ce01ac2c315d8e4a21ac836804e592
ethien <ethienduckett@outlook.com>
2022-04-30 20:13:15 -0400

    I created my autobiography and indexed it. I made the styles look better. I added an image for my autobiography.

--- controller.js
+++ controller.js
@@ -130,7 +130,7 @@ function prevPage(){
 		blog.children[1].innerHTML = blog_data["hook"];
 		blog.children[2].children[0].innerHTML = blog_data["creation_time"];
 		let start_file = blog_data["start_file"];
-		blog.children[2].children[1].innerHTML = `<a href="${start_file}">${re.exec(start_file)}</a>`;
+		blog.children[2].children[1].innerHTML = `<a href="${start_file}" target="_blank">${re.exec(start_file)}</a>`;
 		index--;
 		if (index < 0) {
 			setIndex(0);
